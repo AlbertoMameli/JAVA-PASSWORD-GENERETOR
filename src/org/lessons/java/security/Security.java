4cae320aa82dd4213e38abd04e2c79435a05582d
AlbertoMameli <mamelialberto2@gmail.com>
2025-04-12 19:40:29 +0200

    feat aggiunto valore randomico

--- src/org/lessons/java/security/Security.java
+++ src/org/lessons/java/security/Security.java
@@ -1,5 +1,6 @@
 package org.lessons.java.security;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Security {
@@ -13,8 +14,8 @@ public class Security {
         System.out.println("Inserisci in tuo cognome");
         String cognome = input.nextLine();
 
-        nome = nome.substring(0, 1).toUpperCase() + nome.substring(1);
-        cognome = cognome.substring(0, 1).toUpperCase() + cognome.substring(1);
+        nome = nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase();
+        cognome = cognome.substring(0, 1).toUpperCase() + cognome.substring(1).toLowerCase();
 
         System.out.println("dimmi il tuo colore preferito");
         String colorePreferito = input.nextLine();
@@ -30,13 +31,19 @@ public class Security {
         int annoNascita = input.nextInt();
 
         int sommaData = giornoNascita + meseNascita + annoNascita;
+// aggiungo generatore numero randomico
+        Random numero = new Random();
+        int max = 5000;
+        int result = numero.nextInt(max);
+        System.out.println("numero randomico =" + result);
+
+        int randomData = result + sommaData;
 
         // Generazione della password concatenando i dati
-        String password = nome + "-" + cognome + "-" + colorePreferito + "-" + sommaData;
+        String password = nome + "-" + cognome + "-" + colorePreferito + "-" + randomData;
 
         // Output della password
         System.out.println("La tua password generata è: " + password);
-      
 
         input.close();
     }
